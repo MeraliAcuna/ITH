c89306804ae83f1d30c802dbba08d73956f92275
MeraliAcuna <112598438+MeraliAcuna@users.noreply.github.com>
2022-11-03 08:54:02 -0700

    03/11/2022

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { AppService } from './app.service';
 import { UserController } from './users/user/user.controller';
 import { UserService } from './users/user/user.service';
 import { SalesModule } from './api/Users/user/user.module';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [Connection, UserModule],
+  imports: [ConfigModule.forRoot({envFilePath: ProcessingInstruction.env.NODE_ENV==='docker' ? '.env.docker' : '.env.local'})], 
+  Connection, UserModule, SalesModule
   controllers: [AppController],
   providers: [AppService],
 })
